64fc5a71713256d42dd4b8e48f05d8d6cdfb0633
KirstineBerg <kirstine.berg4@gmail.com>
2022-02-09 12:33:55 +0100

    added button component, and some dynamic view creation

--- app/src/main/java/com/bignerdranch/android/gamecreator/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/gamecreator/MainActivity.java
@@ -2,11 +2,14 @@ package com.bignerdranch.android.gamecreator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
     private EditText passwordAnswer_editText;
     private Button submit_button;
     private Button submit_answer_button;
+
+    private EditText dynamicEdit;
+    private Button dynamicButton;
+
+    private LinearLayout ll;
 
     private String password;
 
@@ -28,6 +36,33 @@ public class MainActivity extends AppCompatActivity {
         passwordAnswer_editText = findViewById(R.id.password_text_answer);
         submit_button = findViewById(R.id.password_button);
         submit_answer_button = findViewById(R.id.password_button_answer);
+
+        dynamicEdit = findViewById(R.id.dynamicTestText);
+        dynamicButton = findViewById(R.id.dynamicButton);
+        ll = findViewById(R.id.linearLayoutTest);
+
+        /*LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+*/
+        dynamicButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Context context = v.getContext();
+                String prompt = dynamicEdit.getText().toString().toLowerCase().trim();
+                switch (prompt) {
+                    case "text":
+                        TextView text = new TextView(context);
+                        text.setText("Some text");
+                        ll.addView(text);
+                        break;
+                    case "button":
+                        Button button = new Button(context);
+                        button.setText("Some button");
+                        ll.addView(button);
+                        break;
+                    default:
+                }
+            }
+        });
 
         submit_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
