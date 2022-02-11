33e61d13a9ca94caa9ef619436a32a72b2f5b526
Smilla <smdi@itu.dk>
2022-02-11 11:40:04 +0100

    Add TextComponent class

---
+++ app/src/main/java/com/bignerdranch/android/gamecreator/TextComponent.java
@@ -0,0 +1,19 @@
+package com.bignerdranch.android.gamecreator;
+
+import android.widget.TextView;
+
+public class TextComponent extends GameComponent {
+
+    private TextView textView;
+    private String text;
+
+    public TextComponent(int id, String text) {
+        super(id);
+        this.text = text;
+        textView.setText(text);
+    }
+
+    public TextView getTextView() {
+        return textView;
+    }
+}
