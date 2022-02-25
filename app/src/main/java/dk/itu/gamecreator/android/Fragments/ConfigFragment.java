93ad443509863349c4507cbb43169754566d40d4
Smilla <smdi@itu.dk>
2022-02-25 14:23:08 +0100

    Add fragment for config page

---
+++ app/src/main/java/dk/itu/gamecreator/android/Fragments/ConfigFragment.java
@@ -0,0 +1,49 @@
+package dk.itu.gamecreator.android.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.fragment.app.Fragment;
+import dk.itu.gamecreator.android.R;
+
+public class ConfigFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_config, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Difficulty setting
+        Spinner difficultySpinner = view.findViewById(R.id.difficulty_spinner);
+        ArrayAdapter<CharSequence> difficultyAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.difficulty_array, android.R.layout.simple_spinner_item);
+        difficultyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        difficultySpinner.setAdapter(difficultyAdapter);
+
+        // Color theme setting
+        Spinner colorSpinner = view.findViewById(R.id.color_spinner);
+        ArrayAdapter<CharSequence> colorAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.color_array, android.R.layout.simple_spinner_item);
+        colorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        colorSpinner.setAdapter(colorAdapter);
+
+        // Number of tries setting
+        Spinner noTriesSpinner = view.findViewById(R.id.no_tries_spinner);
+        ArrayAdapter<CharSequence> noTriesAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.no_tries_array, android.R.layout.simple_spinner_item);
+        noTriesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        noTriesSpinner.setAdapter(noTriesAdapter);
+    }
+}
