5a4b09df848a48c3aef02561959b20ce31442372
KirstineBerg <kirstine.berg4@gmail.com>
2022-03-04 11:20:04 +0100

    location

--- app/src/main/java/dk/itu/gamecreator/android/Activities/MapActivity.java
+++ app/src/main/java/dk/itu/gamecreator/android/Activities/MapActivity.java
@@ -1,10 +1,17 @@
 package dk.itu.gamecreator.android.Activities;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.CustomZoomButtonsController;
 import org.osmdroid.views.MapView;
+
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -15,17 +22,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.util.ArrayList;
 
 import dk.itu.gamecreator.android.R;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements LocationListener {
 
     private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
     private MapView map = null;
+    private MyLocationNewOverlay mLocationOverlay;
+
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    protected double latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +54,63 @@ public class MapActivity extends AppCompatActivity {
 
         map = findViewById(R.id.mapView);
         map.setTileSource(TileSourceFactory.MAPNIK);
+        map.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.ALWAYS);
+        map.setMultiTouchControls(true);
 
-
-        requestPermissionsIfNecessary(new String[] {
+        requestPermissionsIfNecessary(new String[]{
                 // if you need to show the current location, uncomment the line below
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 // WRITE_EXTERNAL_STORAGE is required in order to show the map
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
         });
+
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
+        mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this), map);
+        mLocationOverlay.enableMyLocation();
+        map.getOverlays().add(this.mLocationOverlay);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(9.5);
+        GeoPoint p = mLocationOverlay.getMyLocation();
+        System.out.println("lat: " + p.getLatitude() + ", lon: " + p.getLongitude());
+        //GeoPoint startPoint = new GeoPoint(latitude, longitude);
+        mapController.setCenter(p);
+        mapController.animateTo(p);
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
